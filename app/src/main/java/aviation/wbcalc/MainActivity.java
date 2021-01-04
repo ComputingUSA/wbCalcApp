8cd8852942aebdb05544c9a50158a9b905561bfd
ComputingUSA <peter.ungsunan@gmail.com>
2021-01-04 14:02:59 -0500

    initial commit

---
+++ app/src/main/java/aviation/wbcalc/MainActivity.java
@@ -0,0 +1,15 @@
+package aviation.wbcalc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
